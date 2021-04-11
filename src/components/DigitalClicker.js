6fdf15c3e54d3feaf00e5c2c5e856f530ea0b593
hamza15 <sardan5@vt.edu>
2021-04-10 21:29:13 -0400

    commit tree 742344954da1f748c219347464896f010236fc9b

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,27 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+export default class DigitalClicker extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            timesClicked: 0
+        };
+    }
+
+    handleState = () => {
+        let newVal = this.state.timesClicked + 1
+        this.setState({
+            timesClicked: newVal
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <button onClick={this.handleState}>{this.state.timesClicked}</button>
+            </div>
+        )
+    }
+}
